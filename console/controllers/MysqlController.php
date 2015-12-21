293e5af73d1c637665576c947a06f0fe4bd0507d
Tobias Munk <schmunk@usrbin.de>
2015-12-21 10:53:46 +0100

    added create action from app-command

--- console/controllers/MysqlController.php
+++ console/controllers/MysqlController.php
@@ -2,6 +2,7 @@
 
 namespace dmstr\console\controllers;
 
+use igorw\FailingTooHardException;
 use mikehaertl\shellcommand\Command;
 use yii\console\Controller;
 use yii\console\Exception;
@@ -15,6 +16,8 @@ use yii\console\Exception;
  */
 class MysqlController extends Controller
 {
+    public $mysqlRetryMaxCount = 20;
+    public $mysqlRetryTimeout = 2;
     public $noDataTables = [];
 
     /**
@@ -35,6 +38,81 @@ class MysqlController extends Controller
             ' -u ' . getenv('DB_ENV_MYSQL_USER') .
             ' --password=' . getenv('DB_ENV_MYSQL_PASSWORD') . ' ' . getenv('DB_ENV_MYSQL_DATABASE');
         $this->stdout($this->execute($cmd));
+        $this->stdout("\n");
+    }
+
+    /**
+     * create MySQL database from ENV vars and grant permissions
+     *
+     * Note: skips creation, if root password is not set
+     *
+     * @param $db database name
+     */
+    public function actionCreate($db = null)
+    {
+        if ($db === null) {
+            $db = getenv("DATABASE_DSN_DB");
+        }
+        if (empty($db)) {
+            $this->stdout('No database configured, skipping setup.');
+            return;
+        }
+
+        $root = getenv("DB_ENV_MYSQL_ROOT_USER") ?: 'root';
+        $root_password = getenv("DB_ENV_MYSQL_ROOT_PASSWORD");
+        if (empty($root_password)) {
+            return;
+        }
+        $user = getenv("DB_ENV_MYSQL_USER");
+        $pass = getenv("DB_ENV_MYSQL_PASSWORD");
+        $dsn = getenv("DATABASE_DSN_BASE");
+
+        $this->stdout(
+            "Checking database connection on DSN '{$dsn}' with user '{$root}'"
+        );
+
+        // trying to connect to database with PDO (20 times, interval 1 second)
+        try {
+            // retry an operation up to 20 times
+            $pdo = \igorw\retry(
+                $this->mysqlRetryMaxCount,
+                function () use ($dsn, $root, $root_password) {
+                    $this->stdout('.');
+                    sleep($this->mysqlRetryTimeout);
+                    return new \PDO($dsn, $root, $root_password);
+                }
+            );
+        } catch (FailingTooHardException $e) {
+            $this->stdout("\n\nError: Unable to connect to database '" . $e->getMessage() . "''");
+            \Yii::$app->end(1);
+        }
+        $this->stdout(' [OK]');
+
+
+        $this->stdout(
+            "\nCreating database '{$db}' and granting permissions to user '{$user}' on DSN '{$dsn}' with user '{$root}'"
+        );
+
+        try {
+            // retry an operation up to 20 times
+            \igorw\retry(
+                $this->mysqlRetryMaxCount,
+                function () use ($dsn, $root, $root_password, $pdo, $user, $pass, $db) {
+                    $pdo->exec(
+                        "CREATE DATABASE IF NOT EXISTS `$db`;
+                 GRANT ALL ON `$db`.* TO '$user'@'%' IDENTIFIED BY '$pass';
+                 FLUSH PRIVILEGES;"
+                    );
+                    $this->stdout('.');
+                    sleep($this->mysqlRetryTimeout);
+                }
+            );
+        } catch (FailingTooHardException $e) {
+            $this->stdout("\n\nError: Unable to setup database '" . $e->getMessage() . "''");
+            \Yii::$app->end(1);
+        }
+
+        $this->stdout(' [OK]');
         $this->stdout("\n");
     }
 
